a9fdb2708f21664b5766edddd93fd65ae5b38d73
ElizabethRF <eli_04nov@hotmail.com>
2017-11-16 10:04:20 -0600

    Login
    parte del login completa
    contraseña ahora está encriptada

--- Rails/automata/app/models/user.rb
+++ Rails/automata/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+	validates :name, presence: true
+    validates :lastname1, presence: true
+    validates :lastname2, presence: true
+    validates :tipo, presence: true
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,uniqueness: {case_sensitive:false}, format: { with: VALID_EMAIL_REGEX}
+    
+    has_secure_password
 end
