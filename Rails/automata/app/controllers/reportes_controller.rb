0efab9b218b405832d7bd79a8ecdaa9d38e629f4
ElizabethRF <eli_04nov@hotmail.com>
2017-11-25 07:13:08 -0600

    Link reportes

---
+++ Rails/automata/app/controllers/reportes_controller.rb
@@ -0,0 +1,6 @@
+class ReportesController < ApplicationController
+   def new
+   end 
+   def index
+   end 
+end
--- Rails/automata/config/routes.rb
+++ Rails/automata/config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+
   resources :users
-  resources :pacientes
+  resources :reportes 
+
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
