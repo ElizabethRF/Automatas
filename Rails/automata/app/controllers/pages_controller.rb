1672c1d229b2189008735e039e650dbaf710613a
ElizabethRF <eli_04nov@hotmail.com>
2017-11-26 14:40:20 -0600

    UPDATED enfermera view

--- Rails/automata/app/controllers/pages_controller.rb
+++ Rails/automata/app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
    def home
+   		if current_user.tipo == 'Enfermera'
+   				redirect_to new_interaction_path
+		end   	
+
    		if !logged_in?
-			redirect_to login_path
+				redirect_to login_path
 		end
    end 
 end
