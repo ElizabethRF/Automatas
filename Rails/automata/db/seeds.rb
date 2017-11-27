9466e8e48652a5eb63237e429a7b70415f5c6ad7
Brandonitas <angiebrand10@hotmail.com>
2017-11-27 02:04:27 -0600

    Script de SQL para la creación de la BD en seeds.rb

--- Rails/automata/db/seeds.rb
+++ Rails/automata/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Brandon", lastname1: "Reyes", lastname2: "Minero", email: "admin@hotmail.com", password: "prueba", tipo: "Administrador")
+
+User.create(name: "Luis", lastname1: "Lucatero", lastname2: "Villanueva", email: "jefe@hotmail.com", password: "prueba", tipo: "Jefe de departamento")
+
+User.create(name: "Eli", lastname1: "Rodriguez", lastname2: "Fallas", email: "medico@hotmail.com", password: "prueba", tipo: "Medico")
+
+User.create(name: "Dafne", lastname1: "Lecona", lastname2: "Cisneros", email: "enfermera@hotmail.com", password: "prueba", tipo: "Enfermera")
+
+User.create(name: "Lety", lastname1: "Padilla", lastname2: "Perez", email: "enfermera2@hotmail.com", password: "prueba", tipo: "Enfermera")
+
+Paciente.create(name: "Ivan", lastname1: "Islas", lastname2: "Juarez", padecimiento: "Pie roto", fechaIngreso: 2017-10-26, sexo: "Masculino", observaciones: "Pie muy roto", fechaDeNacimiento: 1995-11-26) 
+
+Paciente.create(name: "Karla", lastname1: "Robledo", lastname2: "Bandala", padecimiento: "Neurotica", fechaIngreso: 2017-10-15, sexo: "Femenino", observaciones: "Alusinaciones", fechaDeNacimiento: 1997-11-26) 
+
+
